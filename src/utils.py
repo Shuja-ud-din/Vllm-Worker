3817db6d2a6754e5207d3ff8a8af670bc041c9a4
Shuja-ud-din <shuja1339@gmail.com>
2026-02-05 19:29:45 +0500

    chore: fixed build error and used correct base image

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
 from typing import List
 from transformers import AutoTokenizer
-from .models import ChatMessage, ErrorResponse
+from models import ChatMessage, ErrorResponse
 
 def get_tokenizer(model_name: str):
     return AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
